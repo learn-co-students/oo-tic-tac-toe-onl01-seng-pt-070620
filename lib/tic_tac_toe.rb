29fc0bef6095e5d08fd7ed83738aa3e1f77c370c
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-28 20:22:27 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,7 +24,19 @@ class TicTacToe
   def input_to_index(user_input)
     user_input.to_i - 1
   end
+  
+  def move(position, char)
+    @board[position] = char
+  end
 
+  def position_taken?(index_i)
+    ((@board[index_i] == "X") || (@board[index_i] == "O"))
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
@@ -32,4 +44,41 @@ class TicTacToe
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
+  
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
+  end
+  def won?
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
+    end
+      false
+  end
+  end
+  
+  def full?
+  @board.all?{|occupied| occupied != " "}
+  end
+  
+  def draw?
+  !(won?) && (full?)
+  end
+  
+  def over?
+  won? || full? || draw?
+  end
+  
+  
 end
