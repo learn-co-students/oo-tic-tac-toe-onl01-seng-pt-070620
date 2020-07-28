b0c5831f5529eb702c9609470b2cffd34ba225aa
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-28 21:00:08 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -80,5 +80,20 @@ class TicTacToe
   won? || full? || draw?
   end
   
+  def winner
+    if won?
+      @board[won?[0]]
+    end
+  end
   
+def play
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 end
