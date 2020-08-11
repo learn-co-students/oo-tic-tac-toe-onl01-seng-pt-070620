81927138e66834c667d99a00bc9fe52346d4be71
Cory Marker <cmarker4@gmail.com>
2020-08-11 16:17:24 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,6 @@ class TicTacToe
   end
 
   def valid_move?(move)
-
     if move.between?(0, 8) && !position_taken?(move) #(@board[move] == " " || @board[move].nil?)
       true
     else
@@ -64,9 +63,57 @@ class TicTacToe
   end
 
   def turn
-    valid_move?(input_to_index(gets))
-    current_player
-    display_board
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
   end
 
+  def won?
+    x = WIN_COMBINATIONS.find{ |combo|
+    @board[combo[0]] == "X" &&
+    @board[combo[1]] == "X" &&
+    @board[combo[2]] == "X"}
+
+    o = WIN_COMBINATIONS.find{ |combo|
+      @board[combo[0]] == "O" &&
+      @board[combo[1]] == "O" &&
+      @board[combo[2]] == "O"}
+
+    return x || o
+  end
+
+  def full?
+    !@board.any?{|x| x == "" || x == " "}
+  end
+
+  def draw?
+    !won? && full?
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    if won?
+      @board[won?[0]] == "X" ? "X" : "O"
+    else
+      nil
+    end
+  end
+
+  def play
+    @board = Array.new(9, " ")
+    turn until over?
+    # won? ? puts("Congrats #{winner}!") : puts("Nada!")
+    puts "Would you like to play another? (Y or N)"
+    gets.strip.downcase == "y" ||
+    gets.strip.downcase == "yes" ?
+    play : puts("Goodbye!")
+  end
 end
